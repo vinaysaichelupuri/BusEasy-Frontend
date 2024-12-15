3e9d211a82fa3ed638232ea387ef0ab7650bfe2d
Vinay Sai Chelupuri <vinaysaichelupuri@VinaySais-MacBook.local>
2024-12-15 21:48:44 +0530

    Update to useForm from useState

--- src/components/registerPage/Register.tsx
+++ src/components/registerPage/Register.tsx
@@ -1,78 +1,71 @@
-import React, { useState } from "react";
+import React from "react";
 import './Register.css'; 
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 export const Register = () => {
-  const [formData, setFormData] = useState({ username: "", password: "" , email:"",phoneNumber:""});
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+  const { register, handleSubmit } = useForm();
   const navigate = useNavigate();
+
   const goToLogin = () => {
     navigate('/login');
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  // Submit function
+  const onSubmit = async (data) => {
     const baseUrl = process.env.REACT_APP_API_BASE_URL;
-    const response =  await axios.post(`${baseUrl}users`, {
-      name: formData.username,
-      password: formData.password,
-      email:formData.email,
-      phoneNumber:formData.phoneNumber
-    });
-    if(response.status===200){
-        alert('Registration completed succesfully')
+      const response = await axios.post(`${baseUrl}users`, {
+        name: data.username,
+        password: data.password,
+        email: data.email,
+        phoneNumber: data.phoneNumber
+      });
+
+      if (response.status === 200) {
+        alert('Registration completed successfully');
         navigate('/login');
-    }
+      }
   };
 
   return (
-    <div className="login-page ">
+    <div className="login-page">
       <div className="login-container">
         <h1>Welcome to BusEasy!</h1>
-        <h2 >Sign up</h2>
-        <form onSubmit={handleSubmit}>
+        <h2>Sign up</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <input
             type="text"
-            name="username"
             placeholder="Username"
-            value={formData.username}
-            onChange={handleChange}
-            required
+            {...register('username', { required: true })}
           />
-          <input
-            name="password"
-            placeholder="Password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-           <input
-            type="text"
-            name="email"
-            placeholder="Email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-           <input
-            type="text"
-            name="phoneNumber"
-            placeholder="Phone Number"
-            value={formData.phoneNumber}
-            onChange={handleChange}
-            required
-          />
-          <button type="submit" className="login-login-button">Register</button>
-          <div className="login-register-button-div">
-          <button className="login-register-button" onClick={goToLogin}>
-            Already have an account? Login.
-          </button>
-          </div>
 
+          <input
+            type="password"
+            placeholder="Password"
+            {...register('password', { required: true })} 
+          />
+
+          <input
+            type="email"
+            placeholder="Email"
+            {...register('email', { 
+              required: true})} 
+          />
+
+          <input
+            type="text"
+            placeholder="Phone Number"
+            {...register('phoneNumber', { required: true })}
+          />
+
+          <button type="submit" className="login-login-button">Register</button>
+
+          <div className="login-register-button-div">
+            <button className="login-register-button" onClick={goToLogin}>
+              Already have an account? Login.
+            </button>
+          </div>
         </form>
       </div>
     </div>
